3bede279b8cf8f7ee34c0950f6987ebaab9aee02
kanishkaanarang <kanishkaanarang@gmail.com>
2025-07-18 21:42:20 +0530

    middleware and cloudinary upload

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,28 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from 'fs';
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary = async (filePath) => {
+    try {
+        if(!filePath) {
+            throw new Error('File path is required for upload');
+        }
+        //upload the file to Cloudinary
+        const response = await cloudinary.uploader.upload(filePath, { resource_type: 'auto' });
+
+        // file had been uploaded successfully
+        console.log('File uploaded successfully to Cloudinary', response.url);
+        return response;
+
+    } catch (error) {
+        fs.unlinkSync(filePath); // remove the file from local storage as the upload failed
+        return null;
+    }
+}
+
+export default uploadOnCloudinary;
